da070ebe1a695000a586826d5a9a366a250d7dca
rskumar780@gmail.com <rskumar780@gmail.com>
2023-10-17 15:42:55 +0530

    Added collections

---
+++ ExceptionHandlingDemo/src/day8/Employee.java
@@ -0,0 +1,34 @@
+package day8;
+
+public class Employee {
+
+	// Fields
+	private int empId;
+	private String name;
+	
+	// Constructor
+	public Employee() {
+		super();
+	}
+	public Employee(int empId, String name) {
+		super();
+		this.empId = empId;
+		this.name = name;
+	}
+	
+	// Getters & Setters
+	public int getEmpId() {
+		return empId;
+	}
+	public void setEmpId(int empId) {
+		this.empId = empId;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+}
